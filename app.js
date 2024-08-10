5496c886f8e91c3491e7c73f4f826a9275f9d17d
Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2024-08-10 16:03:46 -0400

    Pruebas de métodos HTTP

--- app.js
+++ app.js
@@ -6,6 +6,25 @@ app.get('/', (req, res) => {
   res.send('Hola Mundo!');
 });
 
+app.get('/movies', (req, res) => {
+  res.send('GET: obtener listado de películas');
+});
+app.get('/movies/:id', (req, res) => {
+  res.send('GET: obtener película con identificador ' + req.params.id);
+});
+app.post('/movies', (req, res) => {
+  res.send('POST: crear película');
+});
+app.put('/movies/:id', (req, res) => {
+  res.send('PUT: actualizar película con identificador ' + req.params.id);
+});
+app.patch('/movies/:id', (req, res) => {
+  res.send('PATCH: actualizar película parcialmente con identificador ' + req.params.id);
+});
+app.delete('/movies/:id', (req, res) => {
+  res.send('DELETE: eliminar película con identificador ' + req.params.id);
+});
+
 app.listen(port, () => {
   console.log(`Servidor ejecutándose en el puerto ${port}`)
 });
